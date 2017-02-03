cd116de2208785ce264cfe55812ed65cde8bf46c
Maksym Kolesnykov <mv.kolesnikov@1plus1.tv>
2017-02-03 17:06:32 +0200

    added sql-script which create db

---
+++ app/migrations/sql/feedback.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE feedback;
+USE feedback;
+
+CREATE TABLE users (
+    id INT(10) AUTO_INCREMENT,
+    login VARCHAR(50),
+    password CHAR(32),
+    email VARCHAR(100),
+    privileges TINYINT(1) DEFAULT 0,
+    user_id INT(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABlE feed (
+    id INT(10) AUTO_INCREMENT,
+    text TEXT,
+    feed_date DATETIME,
+    is_changed TINYINT(1) DEFAULT 0,
+    image_name VARCHAR(100),
+    visibility TINYINT(1) DEFAULT 1,
+    PRIMARY KEY (id)
+);
